52372b11d50f3e5f2a4171237684741c67c8d2a3
TeamIbnFernas <153733542+TeamIbnFernas@users.noreply.github.com>
2024-01-01 12:57:15 +0300

    Add files via upload

---
+++ codes/USB_SENDER/USB_SENDER.ino
@@ -0,0 +1,73 @@
+#include <usbhid.h>
+#include <hiduniversal.h>
+#include <usbhub.h>
+
+// Satisfy IDE, which only needs to see the include statment in the ino.
+#ifdef dobogusinclude
+#include <spi4teensy3.h>
+#endif
+#include <SPI.h>
+
+#include <NRFLite.h>
+
+#include "hidjoystickrptparser.h"
+
+const static uint8_t RADIO_ID = 1;             // Our radio's id.
+const static uint8_t DESTINATION_RADIO_ID = 0; // Id of the radio we will transmit to.
+const static uint8_t PIN_RADIO_CE = 7;
+const static uint8_t PIN_RADIO_CSN = 8;
+
+struct RadioPacket // Any packet up to 32 bytes can be sent.
+{
+    uint8_t FromRadioId;
+    uint32_t OnTimeMillis;
+    uint32_t FailedTxCount;
+    
+};
+
+NRFLite _radio;
+RadioPacket _radioData;
+
+USB Usb;
+USBHub Hub(&Usb);
+HIDUniversal Hid(&Usb);
+JoystickEvents JoyEvents;
+JoystickReportParser Joy(&JoyEvents);
+
+void setup() {
+        Serial.begin(9600);
+#if !defined(__MIPSEL__)
+        while (!Serial); // Wait for serial port to connect - used on Leonardo, Teensy and other boards with built-in USB CDC serial connection
+#endif
+        if (!_radio.init(RADIO_ID, PIN_RADIO_CE, PIN_RADIO_CSN))
+          {   
+          Serial.println("Cannot communicate with radio");
+          while (1); // Wait here forever.
+          }
+          
+        Serial.println("Start");
+
+        if (Usb.Init() == -1)
+                Serial.println("OSC did not start.");
+
+        delay(200);
+
+        if (!Hid.SetReportParser(0, &Joy))
+                ErrorMessage<uint8_t > (PSTR("SetReportParser"), 1);
+}
+
+void loop() {
+         if (_radio.send(DESTINATION_RADIO_ID, &_radioData, sizeof(_radioData))) // Note how '&' must be placed in front of the variable name.
+    {
+        Serial.println("...Success");
+    }
+    else
+    {
+        Serial.println("...Failed");
+        
+    }
+
+    delay(1000);
+        Usb.Task();
+}
+
---
+++ codes/nrf_send/nrf_send.ino
@@ -0,0 +1,66 @@
+#include <nRF24L01.h>
+#include <printf.h>
+#include <RF24.h>
+#include <RF24_config.h>
+#include <SPI.h>  
+#include "RF24.h"
+byte addresses[][6] = {"0"};
+RF24 myRadio (7, 8); //Initializes the RF24 object with the specified Chip Enable (CE) and Chip Select Not (CSN) pin numbers.
+int xPin = A0;
+int yPin = A1;
+int buttonPin = 4;
+
+
+struct package
+{
+  int id=1;
+  float temperature = 18.3;
+  char  text[100] = "Text to be transmitted";
+};
+
+
+typedef struct package Package;
+Package data;
+
+
+void setup()
+{
+  pinMode(buttonPin, INPUT_PULLUP);
+  Serial.begin(115200);
+  delay(1000);
+  myRadio.begin();  
+  myRadio.setChannel(115); 
+  myRadio.setPALevel(RF24_PA_MAX);
+  myRadio.setDataRate( RF24_250KBPS ) ; 
+  myRadio.openWritingPipe( addresses[0]);
+  
+  delay(1000);
+}
+void loop()
+{
+  int xVal = analogRead(xPin);
+  int yVal = analogRead(yPin);
+  int buttonVal = digitalRead(buttonPin);
+
+  Serial.print("X = ");
+  Serial.print(xVal);
+
+  Serial.print("    Y = ");
+  Serial.print(yVal);
+
+  Serial.print("    Button = ");
+  Serial.println(buttonVal);
+
+  delay(100);
+  myRadio.write(&data, sizeof(data)); 
+
+  Serial.print("\nPackage:");
+  Serial.print(data.id);
+  Serial.print("\n");
+  Serial.println(data.temperature);
+  Serial.println(data.text);
+  data.id = data.id + 1;
+  data.temperature = data.temperature+0.1;
+  delay(1000);
+
+}
