b8371c62d2e9b34f126440d3c3260ef2d546ec05
TeamIbnFernas <153733542+TeamIbnFernas@users.noreply.github.com>
2024-06-06 23:44:35 +0300

    Add files via upload

---
+++ S-joystick/Shit-joystick/RunJoyStickOnLocalMachine/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace RunJoyStickOnLocalMachine
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.joystickTimer = new System.Windows.Forms.Timer(this.components);
+            this.label4 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.progressBar2 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.progressBar3 = new System.Windows.Forms.ProgressBar();
+            this.progressBar4 = new System.Windows.Forms.ProgressBar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // joystickTimer
+            // 
+            this.joystickTimer.Enabled = true;
+            this.joystickTimer.Tick += new System.EventHandler(this.joystickTimer_Tick_1);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(67, 113);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(99, 29);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "R-XAxis";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(67, 37);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(95, 29);
+            this.label8.TabIndex = 0;
+            this.label8.Text = "L-XAxis";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(53, 406);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(452, 159);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(293, 37);
+            this.progressBar1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(187, 28);
+            this.progressBar1.TabIndex = 1;
+            this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
+            // 
+            // progressBar2
+            // 
+            this.progressBar2.Location = new System.Drawing.Point(293, 111);
+            this.progressBar2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.progressBar2.Name = "progressBar2";
+            this.progressBar2.Size = new System.Drawing.Size(187, 28);
+            this.progressBar2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(67, 266);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "R-YAxis";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(67, 190);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 29);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "L-YAxis";
+            // 
+            // progressBar3
+            // 
+            this.progressBar3.Location = new System.Drawing.Point(293, 185);
+            this.progressBar3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.progressBar3.Name = "progressBar3";
+            this.progressBar3.Size = new System.Drawing.Size(187, 28);
+            this.progressBar3.TabIndex = 1;
+            this.progressBar3.Click += new System.EventHandler(this.progressBar1_Click);
+            // 
+            // progressBar4
+            // 
+            this.progressBar4.Location = new System.Drawing.Point(293, 258);
+            this.progressBar4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.progressBar4.Name = "progressBar4";
+            this.progressBar4.Size = new System.Drawing.Size(187, 28);
+            this.progressBar4.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(67, 342);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 29);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Gear";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 5000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(559, 596);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.progressBar4);
+            this.Controls.Add(this.progressBar2);
+            this.Controls.Add(this.progressBar3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label4);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Joystikc";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer joystickTimer;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ProgressBar progressBar2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ProgressBar progressBar3;
+        private System.Windows.Forms.ProgressBar progressBar4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ S-joystick/Shit-joystick/RunJoyStickOnLocalMachine/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+using System.IO;
+using System.Management;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Collections;
+using Sharer;
+using System.IO.Ports;
+
+namespace RunJoyStickOnLocalMachine
+{
+    public partial class Form1 : Form
+    {
+        private Joystick joystick;
+        readonly SharerConnection connection = new SharerConnection("COM8", 115200);
+        int CoarseGear = 0;
+        int FineGear = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            joystick = new Joystick(this.Handle);
+            connectToJoystick(joystick);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            connection.Connect();
+        }
+
+        //---------------------------------------------------------------------
+        private void connectToJoystick(Joystick joystick)
+        {
+            while (true)
+            {
+                string sticks = joystick.FindJoysticks();
+                if (sticks != null)
+                {
+                    if (joystick.AcquireJoystick(sticks))
+                    {
+                        enableTimer();
+                        break;
+                    }
+                }
+            }
+        }
+
+        private void enableTimer()
+        {
+            if (this.InvokeRequired)
+            {
+                BeginInvoke(new ThreadStart(delegate ()
+                {
+                    joystickTimer.Enabled = true;
+                }));
+            }
+            else
+                joystickTimer.Enabled = true;
+        }
+
+        private void joystickTimer_Tick_1(object sender, EventArgs e)
+        {
+            try
+            {
+                
+                joystick.UpdateStatus();
+
+                if (joystick.buttons[4] == true) { 
+                    if((1000 + (CoarseGear * 100) + (FineGear * 10)) > 999) { CoarseGear--; }
+                }
+                if (joystick.buttons[5] == true)
+                {
+                    if ((1000 + (CoarseGear * 100) + (FineGear * 10)) < 2001) { CoarseGear++; }
+                }
+                if (joystick.buttons[6] == true) { 
+                    if((1000 + (CoarseGear * 100) + (FineGear * 10)) > 999) { FineGear--; }
+                }
+                if (joystick.buttons[7] == true)
+                {
+                    if ((1000 + (CoarseGear * 100) + (FineGear * 10)) < 2001) { FineGear++; }
+                }
+                
+                progressBar1.Value = map(joystick.Lx, 0, 65536, progressBar1.Minimum, progressBar1.Maximum);
+                progressBar3.Value = map(joystick.Ly, 0, 65536, progressBar1.Minimum, progressBar1.Maximum);
+                progressBar2.Value = map(joystick.Rx, 0, 65536, progressBar1.Minimum, progressBar1.Maximum);
+                progressBar4.Value = map(joystick.Ry, 0, 65536, progressBar1.Minimum, progressBar1.Maximum);
+                label3.Text =  "Sending PWM = " + (1000 + (CoarseGear * 100)+(FineGear * 10));
+                for (int i = 0; i < 12; i++)
+                {
+                    if (joystick.buttons[i] == true)
+                        richTextBox1.Text += "Button " + i + " pressed\n";
+                }
+                //richTextBox1.Text += "Pressed\n";
+                //for (int i = 0;i < 12; i++)
+                //{
+                //    if (joystick.buttons[i] == false)
+                //        richTextBox1.Text += "Button " + i + ", ";
+                //}
+                //richTextBox1.Text += "NOT Pressed\n";
+                //richTextBox1.Clear();
+            }
+            catch
+            {
+                joystickTimer.Enabled = false;
+                connectToJoystick(joystick);
+            }
+        }
+
+        private static int map(int value, int fromLow, int fromHigh, int toLow, int toHigh)
+        {
+            return (value - fromLow) * (toHigh - toLow) / (fromHigh - fromLow) + toLow;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            connection.WriteVariable("sent_data.ch1", map(joystick.Rx, 0, 65536, 0, 255));
+            connection.WriteVariable("sent_data.ch2", map(joystick.Ry, 0, 65536, 0, 255));
+            //connection.WriteVariable("sent_data.ch3", mapAF(joystick.Ly, 0, 65536, 0, 255));
+            connection.WriteVariable("sent_data.ch4", map(joystick.Lx, 0, 65536, 0, 255));
+            try
+            {
+                connection.WriteVariable("sent_data.ch3", map((1000 + (CoarseGear * 100) + (FineGear * 10)), 2000, 1000, 0, 255));
+            }
+            catch
+            {
+               
+            }
+
+
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
